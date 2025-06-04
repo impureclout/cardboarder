bcb66f89f595a28bf443bbacb35317dd3fea2531
impureclout <impureclout@gmail.com>
2025-06-03 23:07:37 -0400

    Fix: Update vite.config.ts to correctly resolve build paths

--- cardboarder/vite.config.ts
+++ cardboarder/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/cardboarder/',
+  build: {
+    outDir: 'cardboarder/dist',
+    rollupOptions: {
+      input: {
+        main: 'cardboarder/index.html'
+      }
+    },
+  }
 })
