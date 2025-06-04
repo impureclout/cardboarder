f1b5153acc30f523901d0a56952008bbddb23471
impureclout <impureclout@gmail.com>
2025-06-03 23:42:40 -0400

    Refactor: Simplify Vite build and use script to populate /docs

--- cardboarder/vite.config.ts
+++ cardboarder/vite.config.ts
@@ -5,12 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/cardboarder/',
+
   build: {
-    outDir: 'docs',
+    // Vite will build into 'PROJECT_ROOT/cardboarder/dist' because config is in 'cardboarder/'
+    // and input is 'cardboarder/index.html' relative to CWD.
+    // Let's explicitly set outDir to be 'dist' relative to this config file.
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        index: 'cardboarder/index.html'
-      }
+        // This will result in `cardboarder/dist/cardboarder/index.html`
+        // which is fine, as we will move files in the package.json script.
+        main: 'index.html' // This is now relative to the vite.config.ts location
+      },
     },
     emptyOutDir: true,
   }
